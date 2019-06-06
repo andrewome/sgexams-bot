4ba1c4194b614f5a22aa4ee24c3fb445c9133537
andrewome <andrewong3012@gmail.com>
2019-06-06 23:45:52 +0800

    Adding string parsing methods

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,23 @@
-const s: string = "Hello World!";
-console.log(`${s}`);
+import "./lib/env";
+import { Client } from "discord.js";
+import { checkForBannedWords, getContextOfBannedWord } from "./lib/parsingFunctions";
+
+const bot = new Client();
+const bannedWords: string[] = ["shit", "damn"];
+bot.login(process.env.BOT_TOKEN);
+
+bot.on("message", (message) => {
+    // If it's a bot, ignore :)
+    if(message.author.bot)
+        return;
+
+    // Check message contents if it contains a bad word >:o
+    let content = message.content;
+    let bannedWordsFound = checkForBannedWords(content, bannedWords);
+    let contextOfBannedWords: Set<String> = getContextOfBannedWord(content, bannedWordsFound);
+    console.log(contextOfBannedWords);
+})
+
+bot.on('ready', () => {
+  console.log('I am ready!');
+});
