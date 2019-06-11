5230cbb4d4288adf70782e4a2bbfba4ecd5be8ee
andrewome <andrewong3012@gmail.com>
2019-06-11 21:10:46 +0800

    Increased effeciency of making Datamuse API calls.
    Used to wait for each query, now using Promises.all to simultaneously send all queries in parallel and wait for all queries to come back.

--- src/main/message/MessageChecker.ts
+++ src/main/message/MessageChecker.ts
@@ -43,17 +43,21 @@ export class MessageChecker {
 
             //Determine if the contexts of the banned words used was malicious
             let realBannedWords: Context[] = [];
+            let promises: Promise<Context | null>[] = [];
             for(let context of contextOfBannedWords) {
-                try {
-                    let isGuilty = await this.checkContext(context);
-                    if(isGuilty) {
-                        realBannedWords.push(context)
-                    }
-                } catch (err) {
-                    throw err;
-                }
+                promises.push(this.checkContext(context));
             }
-
+            try {
+                let results = await Promise.all(promises);
+                for(let result of results) {
+                    if(result !== null) {
+                        realBannedWords.push(result);
+                    }
+                }
+            } catch (err) {
+                throw err;
+            }
+            
             //Create result and resolve promise
             let isGuilty: boolean;
             if(realBannedWords.length === 0) {
@@ -70,21 +74,21 @@ export class MessageChecker {
      * This function checks datamuse Api for the context of the word used
      * 
      * @param  {Context} context containing banned word used & its context
-     * @return Promise<boolean> if the context is found to be a banned word
+     * @return Promise<Context | null> if the context is found to be a banned word
      */
-    private async checkContext(context: Context): Promise<boolean> {
-        return new Promise<boolean>(async (resolve) => {
+    private async checkContext(context: Context): Promise<Context | null> {
+        return new Promise<Context | null>(async (resolve) => {
             let bannedWord = context.bannedWord;
             let convertedContext = context.convertedContext;
 
             // If it's a perfect match with a banned word, no need to query.
             if(convertedContext === bannedWord) {
-                resolve(true);
+                resolve(context);
             } else {
                 let datamuseQueryResults = await new DatamuseApi().checkSpelling(convertedContext);
                 // If no results, is not a legitimate word, mark it.
                 if(datamuseQueryResults.length === 0) {
-                    resolve(true);
+                    resolve(context);
                 } else {
                     //if it does not match the top few (3 for now), mark it
                     let canBeFound = false;
@@ -100,7 +104,7 @@ export class MessageChecker {
                         
                         //if I can find the banned word in the query, mark it.
                         if(bannedWord === bestFitWord) {
-                            resolve(true)
+                            resolve(context)
                         }
                         idx++;
                     } while(idx < 3 && idx < datamuseQueryResults.length);
@@ -108,11 +112,11 @@ export class MessageChecker {
                     //if it does not match the top few, mark it for now; could be a masked banned word
                     //downside: typos will get flagged too
                     if(!canBeFound) {
-                        resolve(true)
+                        resolve(context)
                     }
                 }
             }
-            resolve(false);
+            resolve(null);
         });
     }
 }
