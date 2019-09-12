62c311676e53cd33cc37dff784071b40c44bce4d
Andrew Ong <39642465+andrewome@users.noreply.github.com>
2019-09-12 20:56:56 +0800

    Updated status check command to return proper plural/non plural strings and made it an embed.

--- src/main/command/statuscheckcommands/StatusCheckCommand.ts
+++ src/main/command/statuscheckcommands/StatusCheckCommand.ts
@@ -1,4 +1,4 @@
-import { Permissions } from 'discord.js';
+import { Permissions, RichEmbed } from 'discord.js';
 import { Command } from '../Command';
 import { Server } from '../../storage/Server';
 import { CommandArgs } from '../classes/CommandArgs';
@@ -6,6 +6,8 @@ import { CommandResult } from '../classes/CommandResult';
 import { StatusCheckCommandData } from './StatusCheckCommandData';
 
 export class StatusCheckCommand extends Command {
+
+    public static EMBED_TITLE = 'Uptime Status:';
 
     /**
      * This method returns the uptime of the bot in hours,
@@ -37,10 +39,27 @@ export class StatusCheckCommand extends Command {
         const upTimeInSeconds = (upTimeInMinutes - roundedUpTimeInMinutes) * 60;
         const roundedUpTimeInSeconds = Math.floor(upTimeInSeconds);
 
-        // Send output
-        messageReply(`Uptime: ${roundedUpTimeInHours} hour(s), ${roundedUpTimeInMinutes} minute(s) and ${roundedUpTimeInSeconds} second(s).`);
+        // Generate and send output
+        const upTimeHoursStr = `${roundedUpTimeInHours} hour` + this.addSIfPlural(roundedUpTimeInHours);
+        const upTimeMinutesStr = `${roundedUpTimeInMinutes} minute` + this.addSIfPlural(roundedUpTimeInMinutes);
+        const upTimeSecondsStr = `${roundedUpTimeInSeconds} second` + this.addSIfPlural(roundedUpTimeInSeconds);
         
-        /* Save servers false, Check messages true*/
+        messageReply(
+            new RichEmbed().setColor(Command.EMBED_DEFAULT_COLOUR)
+                .addField(StatusCheckCommand.EMBED_TITLE, `${upTimeHoursStr}, ${upTimeMinutesStr} and ${upTimeSecondsStr}`)
+        );
+
+        /* Save servers false, Check messages true */
         return new CommandResult(false, true);
+    }
+
+    /**
+     * Literally returns the 's' behind if value is not 1.
+     * 
+     * @param  {number} value
+     * @returns string
+     */
+    private addSIfPlural(value: number): string {
+        return (value === 1) ? '' : 's';
     }
 }
