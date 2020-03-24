51ef1afff45359ad063c30cfe1cefbe2f0845942
Zhao Wei Liew <zhaoweiliew@gmail.com>
2020-03-24 23:57:30 +0800

    Initialize in-memory servers from the SQLite database
    Changes in v2:
    * Split up the logic into logical parts:
      1. Retrieve servers
      2. For each server, retrieve
      	2a. messageCheckerSettings and messageCheckerBannedWords
    	2b. starboardSettings and starboardEmojis
      3. Convert the serialized server settings into actual objects
    * Revive previously removed `convertFromJsonFriendly` helpers
    * Revive previously removed tests for `convertFromJsonFriendly`

--- src/main/storage/Storage.ts
+++ src/main/storage/Storage.ts
@@ -1,14 +1,29 @@
-import * as fs from 'fs';
 import log from 'loglevel';
 import { Server } from './Server';
+import { connect } from '../database';
+
+interface MessageCheckerSettings {
+    bannedWords: string[];
+    deleteMessage: boolean;
+    reportingChannelId: string | null;
+    responseMessage: string | null;
+}
+
+interface StarboardEmoji {
+    id: string;
+    name: string;
+}
+
+interface StarboardSettings {
+    channel: string;
+    threshold: number;
+    emojis: StarboardEmoji[];
+}
 
 /** This represents all the servers that the bot is keeping track of */
 export class Storage {
     /** Map of ID & Server objects */
     public servers: Map<string, Server> = new Map<string, Server>();
-
-    /** Storage path */
-    private STORAGE_PATH = './servers.json'
 
     /**
      * Loads servers from STORAGE_PATH and serialised it into actual objects
@@ -17,46 +32,93 @@ export class Storage {
      */
     public loadServers(): Storage {
         log.info('Loading Servers...');
-        try {
-            const servers = fs.readFileSync(this.STORAGE_PATH, 'utf8');
-            const objects = JSON.parse(servers);
-            for (const object of objects) {
-                const server = Server.convertFromJsonFriendly(object);
-                this.servers.set(server.serverId, server);
-            }
-        } catch (err) {
-            // File not found, create empty file
-            if (err.code === 'ENOENT') {
-                log.info('servers.json not found - creating empty file');
-                fs.writeFileSync(this.STORAGE_PATH, '[]');
-            } else { // Other errors, throw up the chain
-                throw err;
-            }
+        const db = connect();
+
+        // Retrieve servers from the DB
+        const selectServers = db.prepare('SELECT * FROM servers');
+        const servers = selectServers.all();
+
+        for (const { serverId } of servers) {
+            // Load additional settings from the DB
+            const starboardSettings = Storage.getStarboardSettingsFromDb(
+                db,
+                serverId,
+            );
+            const messageCheckerSettings = Storage.getMessageCheckerSettingsFromDb(
+                db,
+                serverId,
+            );
+
+            // Convert to an actual Server object and store it
+            const server = Server.convertFromJsonFriendly({
+                serverId,
+                starboardSettings,
+                messageCheckerSettings,
+            });
+            this.servers.set(server.serverId, server);
         }
         log.info(`Loaded ${this.servers.size} server(s).`);
         return this;
     }
 
     /**
-     * Save servers to STORAGE_PATH
-     * Converts each server object to something that can
-     * be easily JSON.stringified() to.
+     * Returns an object containing the StarboardSettings for the server,
+     * retrieved from the database.
      *
-     * @returns void
+     * @param db The database connection to use.
+     * @param serverId The ID of the server whose settings are to be retrieved.
+     * @returns An object containing the StarboardSettings for the server.
      */
-    public saveServers(): void {
-        const serverJsons = [];
-        log.info('Saving Servers...');
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        for (const [k, v] of this.servers) {
-            log.debug(`Serialising ${k}...`);
-            serverJsons.push(Server.convertToJsonFriendly(v));
+    private static getStarboardSettingsFromDb(db: any, serverId: string): StarboardSettings {
+        // Retrieve settings and emojis from the DB
+        const selectSettings = db.prepare(
+            `SELECT * FROM starboardSettings WHERE serverId = ${serverId}`,
+        );
+        const selectEmojis = db.prepare(
+            `SELECT * FROM starboardEmojis WHERE serverId = ${serverId}`,
+        );
+        const starboardSettings = selectSettings.get();
+        const starboardEmojis = selectEmojis.all();
+
+        // Merge emojis with the main starboardSettings object
+        starboardSettings.emojis = [];
+        for (const emoji of starboardEmojis) {
+            starboardSettings.emojis.push({
+                id: emoji.id,
+                name: emoji.name,
+            });
         }
-        fs.writeFileSync(this.STORAGE_PATH, JSON.stringify(serverJsons));
-        log.info('Saved Servers!');
+
+        return starboardSettings;
     }
 
-    public setStoragePath(str: string): void {
-        this.STORAGE_PATH = str;
+    /**
+     * Returns an object containing the MessageCheckerSettings for the server,
+     * retrieved from the database.
+     *
+     * @param db The database connection to use.
+     * @param serverId The ID of the server whose settings are to be retrieved.
+     * @returns An object containing the MessageCheckerSettings for the server.
+     */
+    private static getMessageCheckerSettingsFromDb(
+        db: any, serverId: string,
+    ): MessageCheckerSettings {
+        // Retrieve settings and banned words from the DB
+        const selectSettings = db.prepare(
+            `SELECT * FROM messageCheckerSettings WHERE serverId = ${serverId}`,
+        );
+        const selectBannedWords = db.prepare(
+            `SELECT * FROM messageCheckerBannedWords WHERE serverId = ${serverId}`,
+        );
+        const messageCheckerSettings = selectSettings.get();
+        const messageCheckerBannedWords = selectBannedWords.all();
+
+        // Merge banned words with the main messageCheckerSettings object
+        messageCheckerSettings.bannedWords = [];
+        for (const bannedWord of messageCheckerBannedWords) {
+            messageCheckerSettings.bannedWords.push(bannedWord.word);
+        }
+
+        return messageCheckerSettings;
     }
 }
